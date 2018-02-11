2f50e4a5e0e9be01499a7de3794ec947eed8ddb0
rvnmrqz <rvnmrqz@gmail.com>
2018-02-11 19:02:38 +0800

    Fixed duplicate row in announcements

--- app/src/main/java/com/example/cedric/alibata/Chapters/Announcement.java
+++ app/src/main/java/com/example/cedric/alibata/Chapters/Announcement.java
@@ -8,7 +8,11 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
@@ -29,7 +33,7 @@ import java.util.Map;
 
 public class Announcement extends AppCompatActivity {
 
-    static ProgressDialog pd;
+
     static CustomAdapter adapter;
     ListView listProduct;
     static ArrayList<Announce> records;
@@ -40,15 +44,23 @@ public class Announcement extends AppCompatActivity {
     static SharedPreferences sharedPreferences;
     static SharedPreferences.Editor editor;
 
+    static LinearLayout layoutLoading;
+    static ScrollView layoutListview;
+    static ProgressBar loadingProgressDialog;
+    static TextView loadingTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_announcement);
 
         staticContext = this;
-        pd = new ProgressDialog(this);
-        pd.setTitle("Loading Announcements");
-        pd.setMessage("Please wait...");
+
+        layoutListview = (ScrollView) findViewById(R.id.layoutList);
+        layoutLoading = (LinearLayout) findViewById(R.id.layoutLoading);
+        loadingProgressDialog = (ProgressBar) findViewById(R.id.loadingProgress);
+        loadingTextView = (TextView) findViewById(R.id.loadingTextView);
+
 
         records = new ArrayList<Announce>();
         header_footer = new View(this);
@@ -65,28 +77,30 @@ public class Announcement extends AppCompatActivity {
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
 
-        sharedPreferences = getSharedPreferences(MySharedPref.SHAREDPREFNAME,MODE_PRIVATE);
+        sharedPreferences = getSharedPreferences(MySharedPref.SHAREDPREFNAME, MODE_PRIVATE);
         editor = sharedPreferences.edit();
 
         loadNotifications();
     }
 
 
+    public static void loadNotifications() {
+        showloading(true, true, "Loading Announcements...");
 
-    public static  void loadNotifications(){
-        showloading(true);
-
-        String url =  "http://alibata-itp.esy.es/App/get_data.php";
+        String url = "http://alibata-itp.esy.es/App/get_data.php";
         StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
                 try {
-                    showloading(false);
-                    System.out.println("Response: "+response);
+
+                    records.clear();
+                    showloading(false, false, null);
+                    System.out.println("Response: " + response);
 
                     JSONArray jArray = new JSONArray(response);
-                    System.out.println("Jarray Length: "+jArray.length());
+                    System.out.println("Jarray Length: " + jArray.length());
 
+                    int ctr =0;
                     for (int i = 0; i < jArray.length(); i++) {
 
                         JSONObject json_data = jArray.getJSONObject(i);
@@ -99,9 +113,14 @@ public class Announcement extends AppCompatActivity {
                         p.setpDate(json_data.getString("Date"));
                         records.add(p);
                         adapter.notifyDataSetChanged(); //notify the ListView to get new records
+                        ctr++;
+                    }
+                    if(ctr==0){
+                        System.out.println("CTR ==0");
+                        showloading(true,false,"No announcements");
                     }
 
-                    editor.putInt(MySharedPref.NOTIFCOUNT,0);
+                    editor.putInt(MySharedPref.NOTIFCOUNT, 0);
                     editor.commit();
 
                 } catch (Exception e) {
@@ -111,18 +130,17 @@ public class Announcement extends AppCompatActivity {
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Toast.makeText(staticContext, "Please Check your Connection",Toast.LENGTH_LONG).show();
+                Toast.makeText(staticContext, "Please Check your Connection", Toast.LENGTH_LONG).show();
                 error.printStackTrace();
-                showloading(false);
+                showloading(true, false, "An Error Occured");
             }
-        })
-        {
+        }) {
             @Override
             protected Map<String, String> getParams() throws AuthFailureError {
                 Map<String, String> params = new HashMap<String, String>();
                 String qry = "select * from announcementtbl order by an_id desc;";
-                System.out.println("Fetching announcement query : "+qry);
-                params.put("qry",qry );
+                System.out.println("Fetching announcement query : " + qry);
+                params.put("qry", qry);
                 return params;
             }
         };
@@ -130,10 +148,21 @@ public class Announcement extends AppCompatActivity {
 
     }
 
-    private  static void showloading(boolean show){
-        if(pd!=null){
-            if(show) pd.show();
-            else pd.hide();
+    private static void showloading(boolean show, boolean showProgressDialog, String message) {
+        System.out.println("showloading: "+show+" showProgressbar: "+showProgressDialog+" message: "+message);
+        if (show) {
+            if (showProgressDialog) loadingProgressDialog.setVisibility(View.VISIBLE);
+            else loadingProgressDialog.setVisibility(View.GONE);
+            loadingTextView.setText(message);
+            layoutListview.setVisibility(View.INVISIBLE);
+            layoutLoading.setVisibility(View.VISIBLE);
+            layoutLoading.bringToFront();
+
+        } else {
+            layoutLoading.setVisibility(View.INVISIBLE);
+            layoutListview.setVisibility(View.VISIBLE);
+            layoutListview.bringToFront();
+
         }
     }
 
@@ -149,14 +178,6 @@ public class Announcement extends AppCompatActivity {
         super.onBackPressed();
     }
 
-    @Override
-    protected void onDestroy() {
-        if(pd!=null){
-            pd.hide();
-            pd.dismiss();
-        }
-        super.onDestroy();
 
-    }
 }
 
